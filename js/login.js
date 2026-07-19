6fa2b97c3834cf42bcd503e2b16a1d8d761c473c
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-07-19 12:30:38 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,43 @@
+// MR Coach Coordination Login System
+// Version 1.0
 
+
+function login(){
+
+let username =
+document.getElementById("username").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+// Demo Login
+// পরে Firebase Authentication যোগ করা হবে
+
+if(username==="admin" && password==="12345"){
+
+
+localStorage.setItem(
+"user",
+"admin"
+);
+
+
+window.location.href="index.html";
+
+
+}
+
+else{
+
+
+document.getElementById("message").innerHTML =
+"❌ Invalid Username or Password";
+
+
+}
+
+
+}
