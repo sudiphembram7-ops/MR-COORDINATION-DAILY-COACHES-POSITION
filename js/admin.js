03e968def08f7f260b2515951329a623c8168dbc
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-07-23 01:14:28 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -53,3 +53,37 @@ function saveCoach() {
     alert("Coach Saved Successfully");
 
 }
+
+function saveCoach(){
+
+let id = column.value + "_" + position.value;
+
+set(
+
+ref(db,"CoachDB/"+id),
+
+{
+
+shop:shop.value,
+
+column:column.value,
+
+position:position.value,
+
+coachNo:coachNo.value,
+
+coachType:coachType.value,
+
+status:status.value,
+
+remarks:remarks.value,
+
+updated:new Date().toLocaleString()
+
+}
+
+);
+
+alert("Coach Saved");
+
+}
