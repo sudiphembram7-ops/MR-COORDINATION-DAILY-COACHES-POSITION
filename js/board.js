2a1737a5654dc1157abc023657f5f58fcaeaff08
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-07-27 16:58:06 +0530

    Update board.js

--- js/board.js
+++ js/board.js
@@ -138,7 +138,9 @@ function updateCoachCell(id, coachNo, status) {
     if (!cell) return;
 
     cell.innerHTML = `
-        <div class="coach-number">${coachNo || "-"}</div>
+        cell.innerHTML = `
+<div class="coach-no">${coach.coachNo}</div>
+<div class="coach-type">${coach.coachType || ""}
         <div class="coach-status">${status || ""}</div>
     `;
 
