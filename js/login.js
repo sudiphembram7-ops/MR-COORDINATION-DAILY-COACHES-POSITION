1137d73b2a04a502b75286c9f24b2434d9185baf
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-07-31 23:22:18 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 import { auth } from "./firebase-config.js";
 
 import {
-  signInWithEmailAndPassword,
-  signOut
+    signInWithEmailAndPassword,
+    signOut
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
-async function login() {
+document.getElementById("loginBtn").addEventListener("click", login);
 
+async function login() {
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
 
     try {
-
         await signInWithEmailAndPassword(auth, email, password);
 
         alert("Login Successful");
@@ -19,28 +19,12 @@ async function login() {
         window.location.href = "admin.html";
 
     } catch (error) {
-
-        console.log(error);
+        console.error(error);
         alert(error.code);
-        alert(error.message);
-
     }
 }
 
-async function logout() {
-
-    try {
-
-        await signOut(auth);
-
-        window.location.href = "login.html";
-
-    } catch (error) {
-
-        console.log(error);
-
-    }
-}
-
-window.login = login;
-window.logout = logout;
+window.logout = async function () {
+    await signOut(auth);
+    window.location.href = "login.html";
+};
