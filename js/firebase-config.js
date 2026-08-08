7b5e868f097e30cc6132d6de5fdb8100dd51efe7
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-08-08 21:37:13 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -21,31 +21,17 @@ import {
    FIREBASE CONFIG
 ========================================== */
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDs7QAXYKv8SSVsrHPZr3Jh9iZ-1qHMEBs",
-
-    authDomain:
-        "mr-coordi-coach.firebaseapp.com",
-
-    databaseURL:
-        "https://mr-coordi-coach-default-rtdb.firebaseio.com",
-
-    projectId:
-        "mr-coordi-coach",
-
-    storageBucket:
-        "mr-coordi-coach.firebasestorage.app",
-
-    messagingSenderId:
-        "955185707268",
-
-    appId:
-        "1:955185707268:web:706c031162f6152565f851"
-
+  apiKey: "AIzaSyDs7QAXYKv8SSVsrHPZr3Jh9iZ-1qHMEBs",
+  authDomain: "mr-coordi-coach.firebaseapp.com",
+  databaseURL: "https://mr-coordi-coach-default-rtdb.firebaseio.com",
+  projectId: "mr-coordi-coach",
+  storageBucket: "mr-coordi-coach.firebasestorage.app",
+  messagingSenderId: "955185707268",
+  appId: "1:955185707268:web:706c031162f6152565f851",
+  measurementId: "G-P6YEBEEWM3"
 };
-
-
 /* ==========================================
    INITIALIZE FIREBASE
 ========================================== */
