cf79aa0683314514c6b8cb7477e56f62a044f08f
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-08-08 21:41:52 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,3 @@
-/* ==========================================
-   firebase-config.js
-   MR CO-ORDINATION
-   FIREBASE CONFIGURATION
-========================================== */
-
 import {
     initializeApp
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
@@ -17,33 +11,36 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
 
-/* ==========================================
-   FIREBASE CONFIG
-========================================== */
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDs7QAXYKv8SSVsrHPZr3Jh9iZ-1qHMEBs",
-  authDomain: "mr-coordi-coach.firebaseapp.com",
-  databaseURL: "https://mr-coordi-coach-default-rtdb.firebaseio.com",
-  projectId: "mr-coordi-coach",
-  storageBucket: "mr-coordi-coach.firebasestorage.app",
-  messagingSenderId: "955185707268",
-  appId: "1:955185707268:web:706c031162f6152565f851",
-  measurementId: "G-P6YEBEEWM3"
+
+    apiKey: "AIzaSyDs7QAXYKv8SSVsrHPZr3Jh9iZ-1qHMEBs",
+
+    authDomain:
+        "mr-coordi-coach.firebaseapp.com",
+
+    databaseURL:
+        "https://mr-coordi-coach-default-rtdb.firebaseio.com",
+
+    projectId:
+        "mr-coordi-coach",
+
+    storageBucket:
+        "mr-coordi-coach.firebasestorage.app",
+
+    messagingSenderId:
+        "955185707268",
+
+    appId:
+        "1:955185707268:web:706c031162f6152565f851",
+
+    measurementId:
+        "G-P6YEBEEWM3"
 };
-/* ==========================================
-   INITIALIZE FIREBASE
-========================================== */
+
 
 const app =
     initializeApp(firebaseConfig);
 
-
-/* ==========================================
-   REALTIME DATABASE
-   EXPLICIT DATABASE URL
-========================================== */
 
 export const database =
     getDatabase(
@@ -52,46 +49,14 @@ export const database =
     );
 
 
-/* ==========================================
-   FIREBASE AUTH
-========================================== */
-
 export const auth =
     getAuth(app);
 
 
-/* ==========================================
-   DEFAULT EXPORT
-========================================== */
-
 export default app;
 
 
-/* ==========================================
-   FIREBASE INITIALIZED
-========================================== */
-
 console.log(
-    "======================================"
-);
-
-console.log(
-    "Firebase App Initialized"
-);
-
-console.log(
-    "Project: mr-coordi-coach"
-);
-
-console.log(
-    "Realtime Database Initialized"
-);
-
-console.log(
-    "Database:",
-    firebaseConfig.databaseURL
-);
-
-console.log(
-    "======================================"
+    "Firebase App Initialized:",
+    firebaseConfig.projectId
 );
