9f11572d079da7719e73f5457dc1eaccca47a007
sudiphembram7-ops <sudiphembram7@gmail.com>
2026-08-21 07:49:33 +0530

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1896,6 +1896,11 @@ function drawBoard() {
         highlightEmptyCells();
 
     }
+    /* =====================================================
+   FINAL COUNTER REFRESH
+===================================================== */
+
+updateCounters();
 
 }
 
